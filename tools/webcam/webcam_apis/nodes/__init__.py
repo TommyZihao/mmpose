a57e4d2f72f1609dec3cbc80fde1716108b3c9d0
zengwang <zengwang430521@gmail.com>
2022-01-16 22:54:30 +0800

    add newyear webcam demo

--- tools/webcam/webcam_apis/nodes/__init__.py
+++ tools/webcam/webcam_apis/nodes/__init__.py
@@ -1,8 +1,10 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .builder import NODES
 from .frame_effect_nodes import (BugEyeNode, NoticeBoardNode,
-                                 PoseVisualizerNode, SunglassesNode)
+                                 PoseVisualizerNode, SunglassesNode,
+                                 FirecrackerNode, HatNode)
 from .faceswap_nodes import FaceSwapNode
+
 from .helper_nodes import ModelResultBindingNode, MonitorNode, RecorderNode
 from .mmdet_nodes import DetectorNode
 from .mmpose_nodes import TopDownPoseEstimatorNode
@@ -10,5 +12,6 @@ from .mmpose_nodes import TopDownPoseEstimatorNode
 __all__ = [
     'NODES', 'PoseVisualizerNode', 'DetectorNode', 'TopDownPoseEstimatorNode',
     'MonitorNode', 'BugEyeNode', 'SunglassesNode', 'ModelResultBindingNode',
-    'NoticeBoardNode', 'RecorderNode', 'FaceSwapNode'
+    'NoticeBoardNode', 'RecorderNode', 'FaceSwapNode', 'FirecrackerNode',
+    'HatNode'
 ]
